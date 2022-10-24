4cd2be73f02d9ddf26e142a6ca43f90acb0a2bba
fazrinmauza1 <94482931+fazrinmauza1@users.noreply.github.com>
2022-10-24 09:13:05 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -94,7 +94,7 @@ async function startjobotz() {
         mek = chatUpdate.messages[0]
         if (!mek.message) return
         mek.message = (Object.keys(mek.message)[0] === 'ephemeralMessage') ? mek.message.ephemeralMessage.message : mek.message
-   if (m.message) return
+   if (!mek.key.fromMe) return
     // if (mek.key && mek.key.remoteJid === 'status@broadcast') return
         if (!jobotz.public && !mek.key.fromMe && chatUpdate.type === 'notify') return
         if (mek.key.id.startsWith('BAE5') && mek.key.id.length === 16) return
